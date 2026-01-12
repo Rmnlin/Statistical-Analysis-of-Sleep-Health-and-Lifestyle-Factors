7193ec90b8bc8678b0b6d47415dab5af23416839
Romnalin <romnalin.than@gmail.com>
2026-01-12 15:23:11 +0700

    Add files via upload

---
+++ main.R
@@ -0,0 +1,13 @@
+# Load and Clean
+source("scripts/01_cleanup.R")
+
+# Exploratory Data Analysis (Graphs)
+source("scripts/02_eda_plots.R")
+
+# Correlation Analysis
+source("scripts/03_correlation.R")
+
+# Statistical Hypothesis Testing
+source("scripts/04_stats_tests.R")
+
+print("Analysis Completed Successfully! Check 'images/' folder for plots.")
